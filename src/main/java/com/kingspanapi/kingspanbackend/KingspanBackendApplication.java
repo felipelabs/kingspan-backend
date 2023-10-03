e20f177ab101285b7fbc2e95f0e3ad6a69174f85
felipe neves <felipe.ferreira@saamauditoria.com.br>
2023-10-02 21:14:06 -0300

    `feat(api)Commit Inicial do projeto:` Adicionando os primeiros arquivos no projeto.

---
+++ src/main/java/com/kingspanapi/kingspanbackend/KingspanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kingspanapi.kingspanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingspanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingspanBackendApplication.class, args);
+	}
+
+}
