60e7137e397934d289ef79c31bae0632220204f6
felipe neves <felipe.ferreira@saamauditoria.com.br>
2023-10-03 20:45:35 -0300

    `feat(api)adição camada Service:` Adicionando regra de negócio com a camada service.

---
+++ src/main/java/com/kingspanapi/kingspanbackend/service/UserService.java
@@ -0,0 +1,23 @@
+package com.kingspanapi.kingspanbackend.service;
+
+import com.kingspanapi.kingspanbackend.entity.User;
+import com.kingspanapi.kingspanbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
