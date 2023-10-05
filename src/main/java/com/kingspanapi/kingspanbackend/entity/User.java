fd403ebb84cc3c67d4ab22fa3c2bfb161ab72b70
felipe neves <felipe.ferreira@saamauditoria.com.br>
2023-10-04 22:09:05 -0300

    `hotfix(api)Entidade user:` Removendo o lombok da entidade User.

--- src/main/java/com/kingspanapi/kingspanbackend/entity/User.java
+++ src/main/java/com/kingspanapi/kingspanbackend/entity/User.java
@@ -1,14 +1,8 @@
 package com.kingspanapi.kingspanbackend.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.*;
 
-
 @Entity
-@Getter
-@Setter
 @Table(name = "table_api")
 public class User {
 
@@ -21,4 +15,36 @@ public class User {
     private String username;
     @Column(length = 50, nullable = false)
     private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
