ba8cf6a85c04232accd18fa7e38edf8c4f11253e
felipe neves <felipe.ferreira@saamauditoria.com.br>
2023-10-04 21:05:13 -0300

    `feat(api)adição CrossOrigin:` Adicionando A Anotação @CrossOrigin para o link do frontend.

--- src/main/java/com/kingspanapi/kingspanbackend/controller/UserController.java
+++ src/main/java/com/kingspanapi/kingspanbackend/controller/UserController.java
@@ -4,14 +4,12 @@ import com.kingspanapi.kingspanbackend.entity.User;
 import com.kingspanapi.kingspanbackend.repository.UserRepository;
 import com.kingspanapi.kingspanbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
